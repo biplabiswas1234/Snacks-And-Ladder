8a3ff82a59751501f9c44e9b043e7566830adc43
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-13 19:25:40 +0530

    UC1[Biplab] Add: Snack and Ladder

---
+++ SnackLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnackLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Snack = 0;
+            Console.WriteLine("Welcome to Snack and Ladder");
+            Console.WriteLine("The player starts with position :" +Snack);
+        }
+    }
+}
